9156d70b4f649d011dd8a2d7a907b5a6fa116eef
HHH <602327249@qq.com>
2025-02-24 21:58:00 +0800

    feat: 虚拟列表优化

--- src/page/visualList.vue
+++ src/page/visualList.vue
@@ -1,17 +1,32 @@
 <template>
-    <p>虚拟滚动列表</p>
-    <!-- 虚拟滚动外层包裹 -->
-    <div ref="visualWarpper" class="visual-list_wrapper">
-        <!-- 垫片 总高度 -->
-        <div class="visual-list_pad" :style="{height: `${totalH}px`}"></div>
-        <!-- 滚动列表 -->
-        <ul class="scroll-list" :style="{transform: `translateY(${scrollTop}px)`}">
-            <li v-for="(list, index) of scrollList" :key="index" class="scroll-list_item">{{list}}</li>
-        </ul>
-    </div>
+  <p>虚拟滚动列表</p>
+  <!-- 虚拟滚动外层包裹 -->
+  <div
+    ref="visualWarpper"
+    class="visual-list_wrapper"
+  >
+    <!-- 垫片 总高度 -->
+    <div
+      class="visual-list_pad"
+      :style="{height: `${totalH}px`}"
+    />
+    <!-- 滚动列表 -->
+    <ul
+      class="scroll-list"
+      :style="{transform: `translateY(${scrollTop}px)`}"
+    >
+      <li
+        v-for="(list, index) of scrollList"
+        :key="index"
+        class="scroll-list_item"
+      >
+        {{ list }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script setup>
-import { ref, reactive, onMounted, onBeforeUnmount } from "vue";
+import {onBeforeUnmount } from "vue";
 
 const originData = [];
 const createList = () => {
@@ -22,6 +37,8 @@ const createList = () => {
 
 const scrollList = reactive([]);
 
+const signleDomHeight = 80;
+
 const totalH = ref(0);
 
 const scrollTop = ref(0);
@@ -31,13 +48,48 @@ const viewContainerH = ref(0);
 const visualWarpper = ref();
 
 const changeScrollList = (scrollTopVal) => {
-    const max = Math.ceil(viewContainerH.value / 80);
-    const start  = Math.floor(scrollTopVal / 80);
-    // 保证scrollList始终置顶
-    scrollTop.value = scrollTopVal
-    changeScrollList.start = start;
-    scrollList.splice(0, max, ...originData.slice(start, start + max))
+    // 当前可视区域最多可显示个数
+    const maxDisplayDom = Math.round(viewContainerH.value / signleDomHeight);
+    const direct = scrollTopVal - changeScrollList.scrollTop > 0 ? 'down' : 'up';
+    const intervalDom = 2;
+    const start  = Math.floor(scrollTopVal / signleDomHeight);
+    changeScrollList.scrollTop  = scrollTopVal;
+    console.log(direct, 'start====',scrollTopVal / signleDomHeight , start, 'changeScrollList.start====', (changeScrollList.start || 0), 'max===', maxDisplayDom)
+    // 保证scrollList始终置顶原理：利用translateY偏移量来代替清除掉的数据所占的高度
+    // 置顶显示的为第二个元素
+    if (direct === 'down'){
+      if (start - (changeScrollList.start || 0) >= intervalDom) {
+        console.log('start - (changeScrollList.start || 0)=====', start - (changeScrollList.start || 0))
+        changeScrollList.start = start - intervalDom + 1;
+        scrollTop.value = scrollTopVal - signleDomHeight * intervalDom;
+
+        // 下拉至底部scrollTop值过大时，导致列表无限循环上下抖动处理
+        if (changeScrollList.start + maxDisplayDom + intervalDom > originData.length)  {
+          changeScrollList.start = originData.length - maxDisplayDom -intervalDom;
+          scrollTop.value = totalH.value - viewContainerH.value;
+        }
+      }
+      // 当start = 2 与 start = 0 得到的列表是一样的
+      scrollList.splice(0, scrollList.length, ...originData.slice(changeScrollList.start ? changeScrollList.start - 1 : changeScrollList.start, changeScrollList.start + maxDisplayDom + intervalDom))
+    } else if(direct === 'up'){
+        // 什么时候同步scrollTopVal
+        // 什么时候加载新数据
+        if (start - changeScrollList.start < intervalDom) {
+          if (changeScrollList.start > 0) {
+            changeScrollList.start--;
+            scrollTop.value = scrollTopVal - signleDomHeight;
+          }
+        }
+        scrollList.splice(0, scrollList.length, ...originData.slice(changeScrollList.start ? changeScrollList.start - 1 : changeScrollList.start, changeScrollList.start + maxDisplayDom + (intervalDom - 1)))
+    }
+
+    if (start < (intervalDom + 1)) {
+        scrollTop.value = 0;
+    }
 }
+
+changeScrollList.start = 0;
+changeScrollList.scrollTop = 0;
 
 const scrollHandler = function(){
     console.log(this.scrollTop)
